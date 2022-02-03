076db69157007c53afbd19f454b218c200cb67cb
Henrique de Freitas Soares <henrique.de.f.soares@gmail.com>
2022-02-03 16:38:07 -0400

    v2.0 Implementação da busca em profundidade Melhoria estética Implementação da validação de entrada Refatoração

--- main.py
+++ main.py
@@ -2,64 +2,134 @@ import os
 import puzzle_ia as ia
 
 
+#Verifica qual é o sistema operacional
 if os.name == 'nt':
     clear_screen = 'cls'
 else:
     clear_screen = 'clear'
 
+
 #Limpa o terminal
 os.system(clear_screen)
 
 
-#Instruções
-print("\n-----------------INSTRUÇÕES-----------------\n")
-
-print("Informe a configuração inicial e final do jogo indicando a sequência de\nnúmeros na ordem da esquerda para a direita e de cima para baixo,\nutilizando o caracter X para representar o espaço em branco.")
+#Instruções iniciais
+print("\n------------------INSTRUÇÕES------------------\n")
+print("Informe a configuração inicial e final do jogo\nindicando a sequência de números na ordem da\nesquerda para a direita e de cima para baixo,\nutilizando o caracter X para representar o\nespaço em branco.")
 print('\n---------------------------------------------')
-print("\nExemplo:")
-ia.imprime_estado([1,2,3,4,'X',5,6,7,8])
-print('\nA configuração acima é representada pela seguinte entrada: 1 2 3 4 X 5 6 7 8')
+
+input('\nPressione ENTER para ver um exemplo\n')
+os.system(clear_screen)
 
 
-#Entradas
-print("\n\n-----------------ENTRADA--------------------")
+#Exemplo
+print("\n-------------------EXEMPLO-------------------\n")
 
+ia.imprime_estado([1,2,3,4,' ',5,6,7,8])
+
+print('\nEntrada: 1 2 3 4 X 5 6 7 8')
+print('\n---------------------------------------------')
+
+input('\nPressione ENTER para começar\n')
+os.system(clear_screen)
+
+
+#Entrada dos dados
+print("\n-----------------ENTRADA--------------------")
+print("\nInforme o número correspondente ao tipo de\nbusca que deseja fazer: ")
+print("\n1 - Busca em largura")
+print("2 - Busca em profundidade")
+tipo_busca = input("\nEscolha: ") #1 = largura, 2 = profundidade
+
+#Valida entrada
+while tipo_busca != '1' and tipo_busca != '2':
+    os.system(clear_screen)
+
+    print("\n-----------------ENTRADA--------------------")
+    print("\nValor inválido, tente novamente.")
+    print("\nInforme o número correspondente ao tipo de\nbusca que deseja fazer: ")
+    print("\n1 - Busca em largura")
+    print("2 - Busca em profundidade")
+    tipo_busca = input("\nEscolha: ") #1 = largura, 2 = profundidade
+
+os.system(clear_screen)
+
+print("\n-----------------ENTRADA--------------------")
 estado_inicial = input("\nInforme o estado inicial do jogo: ")
 estado_inicial = estado_inicial.split()
+
+#Valida entrada
+while len(estado_inicial) != 9 or 'X' not in estado_inicial or ' ' in estado_inicial:
+    os.system(clear_screen)
+
+    print("\n-----------------ENTRADA--------------------")
+    print("\nEntrada inválida, tente novamente.")
+    estado_inicial = input("\nInforme o estado inicial do jogo: ")
+    estado_inicial = estado_inicial.split()
+
+#Troca o X por um espaço
 pos_vazio = estado_inicial.index('X')
 estado_inicial[pos_vazio] = ' '
 
+os.system(clear_screen)
+
+print("\n-----------------ENTRADA--------------------")
 estado_final = input("\nInforme o estado final do jogo: ")
 estado_final = estado_final.split()
+
+#Valida entrada
+while len(estado_final) != 9 or 'X' not in estado_final or ' ' in estado_final:
+    os.system(clear_screen)
+
+    print("\n-----------------ENTRADA--------------------")
+    print("\nEntrada inválida, tente novamente.")
+    estado_final = input("\nInforme o estado final do jogo: ")
+    estado_final = estado_final.split()
+
+#Troca o X por um espaço
 pos_vazio = estado_final.index('X')
 estado_final[pos_vazio] = ' '
 
+print("\nEncontrando solução...")
+
 
 #Solução
-solucao = ia.busca_em_largura(estado_inicial, estado_final)
+if tipo_busca == '1':
+    solucao = ia.busca_em_largura(estado_inicial, estado_final)
+else:
+    solucao = ia.busca_em_profundidade(estado_inicial, estado_final)
+os.system(clear_screen)
 
 
 #Resultados
-print("\n\n-----------------SOLUÇÃO--------------------")
+print("\n-----------------SOLUÇÃO--------------------")
 
-print(f"\nCusto da busca: foram percorridos {solucao.custo_busca} estado(s)")
-print(f"\nCusto da solução: a solução encontrada exige {solucao.custo_caminho} movimento(s)")
+if solucao == None:
+    print('\nNão foi encontrada uma solução para essa configuração!\n\n Verifique os valores de entrada e tente novamente.')
+else:
+    print(f"\nCusto da busca: foram percorridos {solucao.custo_busca} estado(s)")
+    print(f"\nCusto da solução: a solução encontrada exige {solucao.custo_caminho} movimento(s)")
+    print('\n---------------------------------------------')
+    input('\nPressione ENTER para ver o passo a passo\n')
+    
 
-
-input('\n\nPressione ENTER para ver o passo a passo\n')
-os.system(clear_screen)
-
-cont = 1
-for estado in solucao.caminho:
-    print(f'\nEstado {cont}')
-    ia.imprime_estado(estado)
-    input('\n\nPressione ENTER para avançar\n')
+#Passo a passo para resolver (caminho)
     os.system(clear_screen)
 
-    cont += 1
+    cont = 1
+    for estado in solucao.caminho:
+        print(f"\n-----------------ESTADO {cont}------------------\n")
+        ia.imprime_estado(estado)
+        print('\n---------------------------------------------')
+        input('\nPressione ENTER para avançar\n')
+        os.system(clear_screen)
 
-print('\nFIM!')
-ia.imprime_estado(estado)
+        cont += 1
 
-input('\n\nPressione ENTER para sair\n')
+    print("\n-------------------FIM!---------------------\n")
+    ia.imprime_estado(estado)
+
+print('\n---------------------------------------------')
+input('\nPressione ENTER para sair\n')
 os.system(clear_screen)
+exit()
--- puzzle_ia.py
+++ puzzle_ia.py
@@ -9,25 +9,30 @@ class No:
         self.custo_busca = custo_busca
         self.caminho = copy(caminho)
 
+
+#Troca os valores de 2 posições em uma lista
 def swap(list, pos1, pos2):
     list[pos1], list[pos2] = list[pos2], list[pos1]
     return list
 
-
-def expande(no, custo_busca):
+#Aplica as ações possíveis ao no, gerando até 4 filhos
+def expande(no):
     pos_vazio = no.estado.index(' ')
 
-    CIMA = -3
-    BAIXO = 3
+    #TODO: generalizar
+    comprimento = 3
+    altura = 3
+
+    tamanho_puzzle = comprimento * altura
+
+    CIMA = -comprimento
+    BAIXO = comprimento
     ESQUERDA = -1
     DIREITA = 1
 
-    NUM_QUADROS = 9
-    largura = 3
-
     nos_expandidos = []
 
-    #se for possível mover para cima,
+    #Move pra cima, se possível
     if (pos_vazio + CIMA >= 0):
         novo_estado = copy(no.estado)
         swap(novo_estado, pos_vazio, pos_vazio + CIMA)
@@ -35,54 +40,50 @@ def expande(no, custo_busca):
         novo_caminho = copy(no.caminho)
         novo_caminho.append(novo_estado)
         
-        custo_busca += 1
-        novo_no = No(novo_estado, no.custo_caminho + 1, custo_busca, novo_caminho)
+        novo_no = No(novo_estado, no.custo_caminho + 1, -1, novo_caminho)
 
         nos_expandidos.append(novo_no)
 
-    #se for possível mover para a direita
-    if ((pos_vazio + DIREITA) % largura != 0):
+    #Move pra direita, se possível
+    if ((pos_vazio + DIREITA) % comprimento != 0):
         novo_estado = copy(no.estado)
         swap(novo_estado, pos_vazio, pos_vazio + DIREITA)
 
         novo_caminho = copy(no.caminho)
         novo_caminho.append(novo_estado)
         
-        custo_busca += 1
-        novo_no = No(novo_estado, no.custo_caminho + 1, custo_busca, novo_caminho)
+        novo_no = No(novo_estado, no.custo_caminho + 1, -1, novo_caminho)
 
         nos_expandidos.append(novo_no)
 
-    #se for possível mover para baixo
-    if (pos_vazio + BAIXO < NUM_QUADROS):
+    #Move pra baixo, se possível
+    if (pos_vazio + BAIXO < tamanho_puzzle):
         novo_estado = copy(no.estado)
         swap(novo_estado, pos_vazio, pos_vazio + BAIXO)
 
         novo_caminho = copy(no.caminho)
         novo_caminho.append(novo_estado)
         
-        custo_busca += 1
-        novo_no = No(novo_estado, no.custo_caminho + 1, custo_busca, novo_caminho)
+        novo_no = No(novo_estado, no.custo_caminho + 1, -1, novo_caminho)
 
         nos_expandidos.append(novo_no)
 
-    #se for possível mover para a esquerda
-    if ((pos_vazio + ESQUERDA) % largura != largura - 1):
+    #Move pra esquerda, se possível
+    if ((pos_vazio + ESQUERDA) % comprimento != comprimento - 1):
         novo_estado = copy(no.estado)
         swap(novo_estado, pos_vazio, pos_vazio + ESQUERDA)
 
         novo_caminho = copy(no.caminho)
         novo_caminho.append(novo_estado)
         
-        custo_busca += 1
-        novo_no = No(novo_estado, no.custo_caminho + 1, custo_busca, novo_caminho)
+        novo_no = No(novo_estado, no.custo_caminho + 1, -1, novo_caminho)
 
         nos_expandidos.append(novo_no)
 
     return nos_expandidos
 
 
-def busca_em_largura(estado_inicial, estado_final):
+def busca_em_comprimento(estado_inicial, estado_final):
     no_inicial = No(estado_inicial, 0, 1, [estado_inicial])
 
     if (no_inicial.estado == estado_final):
@@ -91,116 +92,191 @@ def busca_em_largura(estado_inicial, estado_final):
     fila = Queue()
     fila.put(no_inicial)
     estados_percorridos = set()
-    estados_percorridos.add(' '.join(no_inicial.estado))
+    estados_percorridos.add(''.join(no_inicial.estado))
     custo_busca = 1
 
     while not fila.empty():
         no_atual = fila.get()
 
-        nos_expandidos = expande(no_atual, custo_busca)
-
-        for no_expandido in nos_expandidos:
+        for no_expandido in expande(no_atual):
             custo_busca += 1
-
-            #print("Expansão:")
-            #print(f"Custo da busca: {no_expandido.custo_busca}")
-            #print(f"Custo da solução: {no_expandido.custo_caminho}")
-            #print(f"Caminho: {no_expandido.caminho}")
 
             if no_expandido.estado == estado_final:
                 no_expandido.custo_busca = custo_busca
                 return no_expandido
 
-            if ' '.join(no_expandido.estado) not in estados_percorridos:
+            if ''.join(no_expandido.estado) not in estados_percorridos:
                 fila.put(no_expandido)
-                estados_percorridos.add(' '.join(no_expandido.estado))
+                estados_percorridos.add(''.join(no_expandido.estado))
 
     return None
 
-def imprime_estado(estado):
-    print('-------------------------')
-    print('|       |       |       |')
-    print(f'|   {estado[0]}   |   {estado[1]}   |   {estado[2]}   |')
-    print('|       |       |       |')
-    print('-------------------------')
-    print('|       |       |       |')
-    print(f'|   {estado[3]}   |   {estado[4]}   |   {estado[5]}   |')
-    print('|       |       |       |')
-    print('-------------------------')
-    print('|       |       |       |')
-    print(f'|   {estado[6]}   |   {estado[7]}   |   {estado[8]}   |')
-    print('|       |       |       |')
-    print('-------------------------')
+#Versão iterativa
+def busca_em_profundidade(estado_inicial, estado_final):
+    pilha = []
+    pilha.append([estado_inicial, 0]) #[estado, nivel]
 
+    custo_busca = 0
+    caminho = []
+    alcancados = set()
+    solucionado = False
+    nivel = -1
 
-#LIXÃO:
+    while len(pilha) != 0:
+        topo = pilha.pop()
+        estado_atual = topo[0]
 
-'''
-busca_em_largura(estado_inicial, estado_final):
-    #constroi o no correspondente ao estado inicial
-    no_inicial = no(estado_inicial, 0, 1, [estado_inicial])
+        nivel_anterior = nivel
+        nivel = topo[1]
 
-    #verifica se o estado inicial é o estado final
-    if (estado_inicial == estado_final):
-        return no_inicial
+        if nivel < nivel_anterior:
+            caminho.pop()
 
-    fila = fila()
-    fila.push(no_inicial)
-    set = {}
-    set.add(no_inicial.estado)
-    custo_busca = 1
-
-    #enquanto a fila não estiver vazia
-    while not fila.empty():
-        no_atual = fila.pop()
-
-        para todos os estados em expandir(no_atual, custo_busca):
+        if ''.join(estado_atual) not in alcancados:
             custo_busca += 1
+            caminho.append(estado_atual)
 
-            se o estado expandido for o estado final,
-                retorne estado expandido
-            
-            se o estado não foi expandido antes,
-                coloque o estado na fila
-                coloque o estado no conjunto de já alcançados
-    return false
+            if estado_atual == estado_final:
+                solucionado = True
+                break
 
-expandir(no, custo_busca):
-    pos_vazio = no.estado.find(' ')
+            alcancados.add(''.join(estado_atual))
+            pos_vazio = estado_atual.index(' ')
+            comprimento = 3
+            altura = 3
+            tamanho_puzzle = comprimento * altura
+            CIMA = -comprimento
+            BAIXO = comprimento
+            ESQUERDA = -1
+            DIREITA = 1
+            nivel_novo = nivel + 1
 
-    nos_expandidos = []
+            #Move pra esquerda, se possível
+            if ((pos_vazio + ESQUERDA) % comprimento != comprimento - 1):
+                novo_estado = copy(estado_atual)
+                swap(novo_estado, pos_vazio, pos_vazio + ESQUERDA)
+                pilha.append([novo_estado, nivel_novo])
+
+            #Move pra baixo, se possível
+            if (pos_vazio + BAIXO < tamanho_puzzle):
+                novo_estado = copy(estado_atual)
+                swap(novo_estado, pos_vazio, pos_vazio + BAIXO)
+                pilha.append([novo_estado, nivel_novo])
+
+            #Move pra direita, se possível
+            if ((pos_vazio + DIREITA) % comprimento != 0):
+                novo_estado = copy(estado_atual)
+                swap(novo_estado, pos_vazio, pos_vazio + DIREITA)
+                pilha.append([novo_estado, nivel_novo])
+
+            #Move pra cima, se possível
+            if (pos_vazio + CIMA >= 0):
+                novo_estado = copy(estado_atual)
+                swap(novo_estado, pos_vazio, pos_vazio + CIMA)
+                pilha.append([novo_estado, nivel_novo])
+
+    if solucionado:
+        return No(caminho[-1], len(caminho) - 1, custo_busca, caminho)
+    else:
+        return None
+
+
+def imprime_estado(estado):
+    print('        -------------------------')
+    print('        |       |       |       |')
+    print(f'        |   {estado[0]}   |   {estado[1]}   |   {estado[2]}   |')
+    print('        |       |       |       |')
+    print('        -------------------------')
+    print('        |       |       |       |')
+    print(f'        |   {estado[3]}   |   {estado[4]}   |   {estado[5]}   |')
+    print('        |       |       |       |')
+    print('        -------------------------')
+    print('        |       |       |       |')
+    print(f'        |   {estado[6]}   |   {estado[7]}   |   {estado[8]}   |')
+    print('        |       |       |       |')
+    print('        -------------------------')
+
+
+#Versão recursiva
+'''
+def profundidade(estado):
+    #Ve se encontrou o estado final
+    if (estado == estado_final):
+        caminho = []
+        caminho.append(estado)
+
+        global custo_busca
+        custo_busca = len(alcancados)
+
+        return caminho
     
-    #se for possível mover para cima,
+    #Vê se o estado já foi alcançado antes
+    if ''.join(estado) in alcancados:
+        return None
+
+    alcancados.add(''.join(estado))
+
+    #setup
+    pos_vazio = estado.index(' ')
+    CIMA = -3
+    BAIXO = 3
+    ESQUERDA = -1
+    DIREITA = 1
+    tamanho_puzzle = 9
+    comprimento = 3
+
+    #Move pra cima, se possível
     if (pos_vazio + CIMA >= 0):
-        novo_estado = no.estado.swap(pos_vazio, pos_vazio + CIMA)
-        
-        novo_no = no(novo_estado, no.custo_caminho + 1, custo_busca, no.caminho + no.estado)
+        novo_estado = copy(estado)
+        swap(novo_estado, pos_vazio, pos_vazio + CIMA)
+        caminho = profundidade(novo_estado)
+        if caminho != None:
+            caminho.insert(0, estado)
+            return caminho
 
-        nos_expandidos.append(novo_no)
+    #Move pra direita, se possível
+    if ((pos_vazio + DIREITA) % comprimento != 0):
+        novo_estado = copy(estado)
+        swap(novo_estado, pos_vazio, pos_vazio + DIREITA)
+        caminho = profundidade(novo_estado)
+        if caminho != None:
+            caminho.insert(0, estado)
+            return caminho
 
-    #se for possível mover para baixo
-    if (pos_vazio + BAIXO < NUM_QUADROS):
-        novo_estado = no.estado.swap(pos_vazio, pos_vazio + BAIXO)
-        
-        novo_no = no(novo_estado, no.custo_caminho + 1, custo_busca, no.caminho + no.estado)
+    #Move pra baixo, se possível
+    if (pos_vazio + BAIXO < tamanho_puzzle):
+        novo_estado = copy(estado)
+        swap(novo_estado, pos_vazio, pos_vazio + BAIXO)
+        caminho = profundidade(novo_estado)
+        if caminho != None:
+            caminho.insert(0, estado)
+            return caminho
 
-        nos_expandidos.append(novo_no)
+    #Move pra esquerda, se possível
+    if ((pos_vazio + ESQUERDA) % comprimento != comprimento - 1):
+        novo_estado = copy(estado)
+        swap(novo_estado, pos_vazio, pos_vazio + ESQUERDA)
+        caminho = profundidade(novo_estado)
+        if caminho != None:
+            caminho.insert(0, estado)
+            return caminho
 
-    #se for possível mover para a esquerda
-    if ((pos_vazio + ESQUERDA) % largura != largura - 1):
-        novo_estado = no.estado.swap(pos_vazio, pos_vazio + ESQUERDA)
-        
-        novo_no = no(novo_estado, no.custo_caminho + 1, custo_busca, no.caminho + no.estado)
+    return None
+    
 
-        nos_expandidos.append(novo_no)
-        
-    #se for possível mover para a direita
-    if ((pos_vazio + DIREITA) % largura != 0):
-        novo_estado = no.estado.swap(pos_vazio, pos_vazio + DIREITA)
-        
-        novo_no = no(novo_estado, no.custo_caminho + 1, custo_busca, no.caminho + no.estado)
+def busca_em_profundidade(estado_inicial_, estado_final_):
+    global estado_inicial
+    estado_inicial = estado_inicial_
+    
+    global estado_final
+    estado_inicial = estado_inicial_
 
-        nos_expandidos.append(novo_no)
+    caminho = profundidade(estado_inicial_)
 
-    return nos_expandidos
+    if caminho != None:
+        no = No(caminho[-1], len(caminho) - 1, custo_busca, caminho)
+
+        return no
+    else:
+        return None
 '''
